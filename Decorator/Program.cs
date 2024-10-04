855da750ede3e83b000ba5afe8b78464157bb167
anhelinashe <shepel5642@gmail.com>
2024-10-04 17:49:39 +0300

    Add project files.

---
+++ Decorator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+interface ICharacter
+{
+    void Attack();
+}
+class BasicCharacter : ICharacter
+{
+    public void Attack()
+    {
+        Console.WriteLine("Character attack");
+    }
+}
+
+class ArmoredCharacter : ICharacter
+{
+    private ICharacter _character;
+    public ArmoredCharacter(ICharacter character)
+    {
+        _character = character;
+    }
+
+    public void Attack()
+    {
+        Console.WriteLine("Attack in rainforced armor");
+        _character.Attack();
+    }
+}
+
+class WeaponCharacter : ICharacter
+{
+    private ICharacter _character;
+
+    public WeaponCharacter(ICharacter character)
+    {
+        _character = character;
+    }
+
+    public void Attack()
+    {
+        Console.WriteLine("Attack with a powerfull weapon");
+        _character.Attack();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ICharacter character = new BasicCharacter();
+        character = new ArmoredCharacter(character);
+        character = new WeaponCharacter(character);
+
+        character.Attack();
+    }
+}
